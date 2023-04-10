45643e16c2318eb88dddc27220724e0cd0353729
Khaid Hoilett <khaid1@hotmail.com>
2023-04-10 12:33:26 -0400

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+document.getElementById("myButton").onclick = function(){
+
+    var myName = document.getElementById("myText1").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText2").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText3").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText4").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText5").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText6").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText7").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText8").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText9").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText10").value;
+    console.log(myName);
+
+    var myName = document.getElementById("myText11").value;
+    console.log(myName);
+
+}
